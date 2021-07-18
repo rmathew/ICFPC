638cdd604240be3210b52eb4c28c9c1a825cb427
Ranjit Mathew <rmathew@gmail.com>
2021-07-18 23:17:46 +0530

    Try restarting search when stuck at a bad solution.

--- 2021/poses/squeeze/solver.go
+++ 2021/poses/squeeze/solver.go
@@ -9,6 +9,11 @@ import (
 
 const (
 	maxStepsToTgtSol = 16
+
+	initTemp        = 1.0
+	tempDecayFactor = 0.9
+	minTemp         = 0.001
+	itersPerTemp    = 1000
 )
 
 type cost int32
@@ -34,6 +39,8 @@ type annealer struct {
 	currSol    *Pose
 	currTemp   float64
 	kBoltzmann float64
+
+	solCosts []cost
 }
 
 func (t *tgtSolSolver) Reset() {
@@ -83,18 +90,25 @@ func (a *annealer) solCost(sol *Pose) cost {
 }
 
 func (a *annealer) randomlyTugOnPoints(sol *Pose) {
-	low, high := getBounds(sol.Vertices)
-	xW, yH := high.X-low.X, high.Y-low.Y
+	nV := len(sol.Vertices)
+
+	// Fiddle with up to 10% of the vertices, but with at least 1 vertex.
+	maxVictims := int(0.1 * float64(nV))
+	const minVictims = 1
+	if maxVictims < minVictims {
+		maxVictims = minVictims
+	}
 
 	// Up to 1% jitter up/down and right/left, but by at least 1 cell.
 	const maxJitterPct float64 = 0.01
+	low, high := getBounds(sol.Vertices)
+	xW, yH := high.X-low.X, high.Y-low.Y
 	maxDispX := math.Max(1.0, maxJitterPct*float64(xW))
 	maxDispY := math.Max(1.0, maxJitterPct*float64(yH))
 
-	nV := len(sol.Vertices)
-	const maxVictims = 5
+	vQ := rand.Perm(nV)
 	for i := 0; i < maxVictims; i++ {
-		vIdx := rand.Intn(nV)
+		vIdx := vQ[i]
 		ovX, ovY := sol.Vertices[vIdx].X, sol.Vertices[vIdx].Y
 
 		nvX := int32(float64(ovX) + (2.0*rand.Float64()-1.0)*maxDispX)
@@ -136,11 +150,37 @@ func (a *annealer) calibrate() {
 		}
 	}
 	dCost := wCost - oCost
-	// This should yield a 50% probability at the highest temperature for
+	// This should yield a 10% probability at the highest temperature for
 	// picking the solution with the worst cost.
-	a.kBoltzmann = float64(dCost) / (math.Log(2.0) * a.currTemp)
+	const worsePct = 0.1
+	a.kBoltzmann = float64(-dCost) / (math.Log(worsePct) * a.currTemp)
+	log.Printf("kB=%0.3f for %0.2f @T=%0.3f [dCost=%d].",
+		a.kBoltzmann, worsePct, a.currTemp, dCost)
+}
 
-	log.Printf("kB=%0.4f, T=%0.4f, dCost=%d.", a.kBoltzmann, a.currTemp, dCost)
+func (a *annealer) shouldGiveUp() bool {
+	if a.currTemp < minTemp {
+		return true
+	}
+	nSols := len(a.solCosts)
+	const nChk = 3
+	if nSols < nChk {
+		return false
+	}
+	for i := nSols - 1; i > nSols-nChk; i-- {
+		if a.solCosts[i] != a.solCosts[i-1] {
+			return false
+		}
+	}
+	return true
+}
+
+func (a *annealer) shouldRestart(cCost, bCost cost, i int) bool {
+	if cCost <= bCost {
+		return false
+	}
+	const waitPct = 0.75
+	return i > int(waitPct*float64(itersPerTemp))
 }
 
 func (a *annealer) Reset() {
@@ -150,7 +190,10 @@ func (a *annealer) Reset() {
 
 	a.done = false
 	a.currSol = &s
-	a.currTemp = 1.0
+	a.currTemp = initTemp
+
+	expSols := math.Ceil(math.Log(minTemp/initTemp) / math.Log(tempDecayFactor))
+	a.solCosts = make([]cost, 0, int(expSols))
 
 	// Remove this to get a deterministic solution for a problem.
 	rand.Seed(time.Now().UnixNano() % math.MaxInt32)
@@ -162,14 +205,14 @@ func (a *annealer) GetNextSolution() *Pose {
 	if a.done {
 		return a.currSol
 	}
-	const minTemp = 0.001
-	if a.currTemp < minTemp {
+	if a.shouldGiveUp() {
 		a.done = true
+		log.Printf("Costs: %v", a.solCosts)
 		return a.currSol
 	}
 
 	currCost := a.solCost(a.currSol)
-	const itersPerTemp = 1000
+	bestSol, bestCost := a.currSol, currCost
 	for i := 0; i < itersPerTemp; i++ {
 		nSol := a.getCandidateSol()
 		nCost := a.solCost(nSol)
@@ -177,9 +220,15 @@ func (a *annealer) GetNextSolution() *Pose {
 			a.currSol = nSol
 			currCost = nCost
 		}
+		if nCost < bestCost {
+			bestSol, bestCost = nSol, nCost
+		}
+		if a.shouldRestart(currCost, bestCost, i) {
+			a.currSol = bestSol
+			currCost = bestCost
+		}
 	}
-	// TODO: Calibrate a suitable value for this decay factor.
-	const tempDecayFactor = 0.9
+	a.solCosts = append(a.solCosts, currCost)
 	a.currTemp *= tempDecayFactor
 
 	return a.currSol
